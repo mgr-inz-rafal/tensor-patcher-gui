2757d3f26d40b4348d3fea25e621d4209b704149
mgr-inz-rafal <rchabowski@gmail.com>
2019-11-16 16:44:11 +0100

    Introduce Constants

---
+++ tensor-patcher-gui/Constants.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace tensor_patcher_gui {
+    public class Constants {
+        // Map
+        public const int MAP_DIMENSION = 12;
+        public const int MAP_DATA_BYTE_COUNT = MAP_DIMENSION * MAP_DIMENSION;
+        public const int TOTAL_MAP_COUNT = 51;
+        public const int TOTAL_MAP_DATA_SIZE = MAP_DATA_BYTE_COUNT * TOTAL_MAP_COUNT;
+        public const int TILE_SIZE = 48;
+
+        // Toolbox
+        public const int TOTAL_BRICKS = 14;
+        public const int TOTAL_BRICK_ROWS = 2;
+        public const int TOOLBOX_TILE_SIZE = 42;
+
+        public static Dictionary<int, Bitmap> brownBricks = new Dictionary<int, Bitmap> {
+            { 0, Properties.Resources.brick05 },
+            { 1, Properties.Resources.brick06 },
+            { 2, Properties.Resources.brick07 },
+            { 3, Properties.Resources.brick08 },
+            { 4, Properties.Resources.brick09 },
+            { 5, Properties.Resources.brick10 },
+            { 6, Properties.Resources.brick11 },
+            { 7, Properties.Resources.brick12 },
+            { 8, Properties.Resources.brick13 },
+            { 9, Properties.Resources.brick14 },
+            { 10, Properties.Resources.brick15 },
+            { 11, Properties.Resources.brick16 },
+            { 12, Properties.Resources.brick17 },
+            { 13, Properties.Resources.brick18 },
+        };
+        public static Dictionary<int, Bitmap> pinkBricks = new Dictionary<int, Bitmap> {
+            { 0, Properties.Resources.brick05p },
+            { 1, Properties.Resources.brick06p },
+            { 2, Properties.Resources.brick07p },
+            { 3, Properties.Resources.brick08p },
+            { 4, Properties.Resources.brick09p },
+            { 5, Properties.Resources.brick10p },
+            { 6, Properties.Resources.brick11p },
+            { 7, Properties.Resources.brick12p },
+            { 8, Properties.Resources.brick13p },
+            { 9, Properties.Resources.brick14p },
+            { 10, Properties.Resources.brick15p },
+            { 11, Properties.Resources.brick16p },
+            { 12, Properties.Resources.brick17p },
+            { 13, Properties.Resources.brick18p },
+        };
+        public static Dictionary<int, Bitmap> blueBricks = new Dictionary<int, Bitmap> {
+            { 0, Properties.Resources.brick05b },
+            { 1, Properties.Resources.brick06b },
+            { 2, Properties.Resources.brick07b },
+            { 3, Properties.Resources.brick08b },
+            { 4, Properties.Resources.brick09b },
+            { 5, Properties.Resources.brick10b },
+            { 6, Properties.Resources.brick11b },
+            { 7, Properties.Resources.brick12b },
+            { 8, Properties.Resources.brick13b },
+            { 9, Properties.Resources.brick14b },
+            { 10, Properties.Resources.brick15b },
+            { 11, Properties.Resources.brick16b },
+            { 12, Properties.Resources.brick17b },
+            { 13, Properties.Resources.brick18b },
+        };
+        public static Dictionary<int, Bitmap> amygdalaBricks = new Dictionary<int, Bitmap> {
+            { 0, Properties.Resources.brick05a },
+            { 1, Properties.Resources.brick06a },
+            { 2, Properties.Resources.brick07a },
+            { 3, Properties.Resources.brick08a },
+            { 4, Properties.Resources.brick09a },
+            { 5, Properties.Resources.brick10a },
+            { 6, Properties.Resources.brick11a },
+            { 7, Properties.Resources.brick12a },
+            { 8, Properties.Resources.brick13a },
+            { 9, Properties.Resources.brick14a },
+            { 10, Properties.Resources.brick15a },
+            { 11, Properties.Resources.brick16a },
+            { 12, Properties.Resources.brick17a },
+            { 13, Properties.Resources.brick18a },
+        };
+        public static Dictionary<byte, Bitmap> caveByteMap = new Dictionary<byte, Bitmap> {
+            { 5, Properties.Resources.brick05 },
+            { 5 + 64, Properties.Resources.brick05a },
+            { 5 + 64 + 64, Properties.Resources.brick05b },
+            { 5 + 64 + 64 + 64, Properties.Resources.brick05p },
+            { 6, Properties.Resources.brick06 },
+            { 6 + 64, Properties.Resources.brick06a },
+            { 6 + 64 + 64, Properties.Resources.brick06b },
+            { 6 + 64 + 64 + 64, Properties.Resources.brick06p },
+            { 7, Properties.Resources.brick07 },
+            { 7 + 64, Properties.Resources.brick07a },
+            { 7 + 64 + 64, Properties.Resources.brick07b },
+            { 7 + 64 + 64 + 64, Properties.Resources.brick07p },
+            { 8, Properties.Resources.brick08 },
+            { 8 + 64, Properties.Resources.brick08a },
+            { 8 + 64 + 64, Properties.Resources.brick08b },
+            { 8 + 64 + 64 + 64, Properties.Resources.brick08p },
+            { 9, Properties.Resources.brick09 },
+            { 9 + 64, Properties.Resources.brick09a },
+            { 9 + 64 + 64, Properties.Resources.brick09b },
+            { 9 + 64 + 64 + 64, Properties.Resources.brick09p },
+            { 10, Properties.Resources.brick10 },
+            { 10 + 64, Properties.Resources.brick10a },
+            { 10 + 64 + 64, Properties.Resources.brick10b },
+            { 10 + 64 + 64 + 64, Properties.Resources.brick10p },
+            { 11, Properties.Resources.brick11 },
+            { 11 + 64, Properties.Resources.brick11a },
+            { 11 + 64 + 64, Properties.Resources.brick11b },
+            { 11 + 64 + 64 + 64, Properties.Resources.brick11p },
+            { 12, Properties.Resources.brick12 },
+            { 12 + 64, Properties.Resources.brick12a },
+            { 12 + 64 + 64, Properties.Resources.brick12b },
+            { 12 + 64 + 64 + 64, Properties.Resources.brick12p },
+            { 13, Properties.Resources.brick13 },
+            { 13 + 64, Properties.Resources.brick13a },
+            { 13 + 64 + 64, Properties.Resources.brick13b },
+            { 13 + 64 + 64 + 64, Properties.Resources.brick13p },
+            { 14, Properties.Resources.brick14 },
+            { 14 + 64, Properties.Resources.brick14a },
+            { 14 + 64 + 64, Properties.Resources.brick14b },
+            { 14 + 64 + 64 + 64, Properties.Resources.brick14p },
+            { 15, Properties.Resources.brick15 },
+            { 15 + 64, Properties.Resources.brick15a },
+            { 15 + 64 + 64, Properties.Resources.brick15b },
+            { 15 + 64 + 64 + 64, Properties.Resources.brick15p },
+            { 16, Properties.Resources.brick16 },
+            { 16 + 64, Properties.Resources.brick16a },
+            { 16 + 64 + 64, Properties.Resources.brick16b },
+            { 16 + 64 + 64 + 64, Properties.Resources.brick16p },
+            { 17, Properties.Resources.brick17 },
+            { 17 + 64, Properties.Resources.brick17a },
+            { 17 + 64 + 64, Properties.Resources.brick17b },
+            { 17 + 64 + 64 + 64, Properties.Resources.brick17p },
+            { 18, Properties.Resources.brick18 },
+            { 18 + 64, Properties.Resources.brick18a },
+            { 18 + 64 + 64, Properties.Resources.brick18b },
+            { 18 + 64 + 64 + 64, Properties.Resources.brick18p },
+            { 131, Properties.Resources.obstacle00 },
+            { 132, Properties.Resources.obstacle01 },
+            { 1, Properties.Resources.ludek1 },
+            { 2, Properties.Resources.amygdala7 },
+            { 0, Properties.Resources.brick_blank },
+        };
+    }
+}
--- tensor-patcher-gui/Form1.cs
+++ tensor-patcher-gui/Form1.cs
@@ -1,165 +1,18 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace tensor_patcher_gui {
     public partial class MainForm : Form {
-        // Map
-        private const int MAP_DIMENSION = 12;
-        private const int MAP_DATA_BYTE_COUNT = MAP_DIMENSION * MAP_DIMENSION;
-        private const int TOTAL_MAP_COUNT = 51;
-        private const int TOTAL_MAP_DATA_SIZE = MAP_DATA_BYTE_COUNT * TOTAL_MAP_COUNT;
-        private const int TILE_SIZE = 48;
-
-        // Toolbox
-        private const int TOTAL_BRICKS = 14;
-        private const int TOTAL_BRICK_ROWS = 2;
-        private const int TOOLBOX_TILE_SIZE = 42;
-
-        // TODO: Move consts and dictionaries to separate file
-        private static Dictionary<int, Bitmap> brownBricks = new Dictionary<int, Bitmap> {
-            { 0, Properties.Resources.brick05 },
-            { 1, Properties.Resources.brick06 },
-            { 2, Properties.Resources.brick07 },
-            { 3, Properties.Resources.brick08 },
-            { 4, Properties.Resources.brick09 },
-            { 5, Properties.Resources.brick10 },
-            { 6, Properties.Resources.brick11 },
-            { 7, Properties.Resources.brick12 },
-            { 8, Properties.Resources.brick13 },
-            { 9, Properties.Resources.brick14 },
-            { 10, Properties.Resources.brick15 },
-            { 11, Properties.Resources.brick16 },
-            { 12, Properties.Resources.brick17 },
-            { 13, Properties.Resources.brick18 },
-        };
-        private static Dictionary<int, Bitmap> pinkBricks = new Dictionary<int, Bitmap> {
-            { 0, Properties.Resources.brick05p },
-            { 1, Properties.Resources.brick06p },
-            { 2, Properties.Resources.brick07p },
-            { 3, Properties.Resources.brick08p },
-            { 4, Properties.Resources.brick09p },
-            { 5, Properties.Resources.brick10p },
-            { 6, Properties.Resources.brick11p },
-            { 7, Properties.Resources.brick12p },
-            { 8, Properties.Resources.brick13p },
-            { 9, Properties.Resources.brick14p },
-            { 10, Properties.Resources.brick15p },
-            { 11, Properties.Resources.brick16p },
-            { 12, Properties.Resources.brick17p },
-            { 13, Properties.Resources.brick18p },
-        };
-        private static Dictionary<int, Bitmap> blueBricks = new Dictionary<int, Bitmap> {
-            { 0, Properties.Resources.brick05b },
-            { 1, Properties.Resources.brick06b },
-            { 2, Properties.Resources.brick07b },
-            { 3, Properties.Resources.brick08b },
-            { 4, Properties.Resources.brick09b },
-            { 5, Properties.Resources.brick10b },
-            { 6, Properties.Resources.brick11b },
-            { 7, Properties.Resources.brick12b },
-            { 8, Properties.Resources.brick13b },
-            { 9, Properties.Resources.brick14b },
-            { 10, Properties.Resources.brick15b },
-            { 11, Properties.Resources.brick16b },
-            { 12, Properties.Resources.brick17b },
-            { 13, Properties.Resources.brick18b },
-        };
-        private static Dictionary<int, Bitmap> amygdalaBricks = new Dictionary<int, Bitmap> {
-            { 0, Properties.Resources.brick05a },
-            { 1, Properties.Resources.brick06a },
-            { 2, Properties.Resources.brick07a },
-            { 3, Properties.Resources.brick08a },
-            { 4, Properties.Resources.brick09a },
-            { 5, Properties.Resources.brick10a },
-            { 6, Properties.Resources.brick11a },
-            { 7, Properties.Resources.brick12a },
-            { 8, Properties.Resources.brick13a },
-            { 9, Properties.Resources.brick14a },
-            { 10, Properties.Resources.brick15a },
-            { 11, Properties.Resources.brick16a },
-            { 12, Properties.Resources.brick17a },
-            { 13, Properties.Resources.brick18a },
-        };
-        private static Dictionary<int, Bitmap> currentBrickSet = brownBricks;
-
-        private static Dictionary<byte, Bitmap> caveByteMap = new Dictionary<byte, Bitmap> {
-            { 5, Properties.Resources.brick05 },
-            { 5 + 64, Properties.Resources.brick05a },
-            { 5 + 64 + 64, Properties.Resources.brick05b },
-            { 5 + 64 + 64 + 64, Properties.Resources.brick05p },
-            { 6, Properties.Resources.brick06 },
-            { 6 + 64, Properties.Resources.brick06a },
-            { 6 + 64 + 64, Properties.Resources.brick06b },
-            { 6 + 64 + 64 + 64, Properties.Resources.brick06p },
-            { 7, Properties.Resources.brick07 },
-            { 7 + 64, Properties.Resources.brick07a },
-            { 7 + 64 + 64, Properties.Resources.brick07b },
-            { 7 + 64 + 64 + 64, Properties.Resources.brick07p },
-            { 8, Properties.Resources.brick08 },
-            { 8 + 64, Properties.Resources.brick08a },
-            { 8 + 64 + 64, Properties.Resources.brick08b },
-            { 8 + 64 + 64 + 64, Properties.Resources.brick08p },
-            { 9, Properties.Resources.brick09 },
-            { 9 + 64, Properties.Resources.brick09a },
-            { 9 + 64 + 64, Properties.Resources.brick09b },
-            { 9 + 64 + 64 + 64, Properties.Resources.brick09p },
-            { 10, Properties.Resources.brick10 },
-            { 10 + 64, Properties.Resources.brick10a },
-            { 10 + 64 + 64, Properties.Resources.brick10b },
-            { 10 + 64 + 64 + 64, Properties.Resources.brick10p },
-            { 11, Properties.Resources.brick11 },
-            { 11 + 64, Properties.Resources.brick11a },
-            { 11 + 64 + 64, Properties.Resources.brick11b },
-            { 11 + 64 + 64 + 64, Properties.Resources.brick11p },
-            { 12, Properties.Resources.brick12 },
-            { 12 + 64, Properties.Resources.brick12a },
-            { 12 + 64 + 64, Properties.Resources.brick12b },
-            { 12 + 64 + 64 + 64, Properties.Resources.brick12p },
-            { 13, Properties.Resources.brick13 },
-            { 13 + 64, Properties.Resources.brick13a },
-            { 13 + 64 + 64, Properties.Resources.brick13b },
-            { 13 + 64 + 64 + 64, Properties.Resources.brick13p },
-            { 14, Properties.Resources.brick14 },
-            { 14 + 64, Properties.Resources.brick14a },
-            { 14 + 64 + 64, Properties.Resources.brick14b },
-            { 14 + 64 + 64 + 64, Properties.Resources.brick14p },
-            { 15, Properties.Resources.brick15 },
-            { 15 + 64, Properties.Resources.brick15a },
-            { 15 + 64 + 64, Properties.Resources.brick15b },
-            { 15 + 64 + 64 + 64, Properties.Resources.brick15p },
-            { 16, Properties.Resources.brick16 },
-            { 16 + 64, Properties.Resources.brick16a },
-            { 16 + 64 + 64, Properties.Resources.brick16b },
-            { 16 + 64 + 64 + 64, Properties.Resources.brick16p },
-            { 17, Properties.Resources.brick17 },
-            { 17 + 64, Properties.Resources.brick17a },
-            { 17 + 64 + 64, Properties.Resources.brick17b },
-            { 17 + 64 + 64 + 64, Properties.Resources.brick17p },
-            { 18, Properties.Resources.brick18 },
-            { 18 + 64, Properties.Resources.brick18a },
-            { 18 + 64 + 64, Properties.Resources.brick18b },
-            { 18 + 64 + 64 + 64, Properties.Resources.brick18p },
-            { 131, Properties.Resources.obstacle00 },
-            { 132, Properties.Resources.obstacle01 },
-            { 1, Properties.Resources.ludek1 },
-            { 2, Properties.Resources.amygdala7 },
-            { 0, Properties.Resources.brick_blank },
-        };
+        private static Dictionary<int, Bitmap> currentBrickSet = Constants.brownBricks;
 
         private byte[] rawMapData;
-        private Button[] toolboxBrickButtons = new Button[TOTAL_BRICKS];
-        private Cave[] caves = new Cave[TOTAL_MAP_COUNT];
-        private Button[] mapTiles = new Button[MAP_DATA_BYTE_COUNT];
+        private Button[] toolboxBrickButtons = new Button[Constants.TOTAL_BRICKS];
+        private Cave[] caves = new Cave[Constants.TOTAL_MAP_COUNT];
+        private Button[] mapTiles = new Button[Constants.MAP_DATA_BYTE_COUNT];
 
         public MainForm() {
             InitializeComponent();
@@ -183,25 +36,25 @@ namespace tensor_patcher_gui {
         }
 
         private void button_SetBrownBrickSet_Click(object sender, EventArgs e) {
-            currentBrickSet = brownBricks;
+            currentBrickSet = Constants.brownBricks;
             RepaintBrickToolset();
             ShowInfo("Brown brick set selected");
         }
 
         private void button_SetPinkBrickSet_Click(object sender, EventArgs e) {
-            currentBrickSet = pinkBricks;
+            currentBrickSet = Constants.pinkBricks;
             RepaintBrickToolset();
             ShowInfo("Pink brick set selected");
         }
 
         private void button_SetBlueBrickSet_Click(object sender, EventArgs e) {
-            currentBrickSet = blueBricks;
+            currentBrickSet = Constants.blueBricks;
             RepaintBrickToolset();
             ShowInfo("Blue brick set selected");
         }
 
         private void button_SetAmygdalaBrickSet_Click(object sender, EventArgs e) {
-            currentBrickSet = amygdalaBricks;
+            currentBrickSet = Constants.amygdalaBricks;
             RepaintBrickToolset();
             ShowInfo("Amygdala-colored brick set selected");
         }
@@ -246,19 +99,19 @@ namespace tensor_patcher_gui {
             const int MAP_DATA_OFFSET = 0xA022;
             var reader = new System.IO.BinaryReader(File.Open(fileName, FileMode.Open));
             reader.BaseStream.Seek(MAP_DATA_OFFSET, SeekOrigin.Begin);
-            rawMapData = reader.ReadBytes(TOTAL_MAP_DATA_SIZE);
+            rawMapData = reader.ReadBytes(Constants.TOTAL_MAP_DATA_SIZE);
         }
 
         private void ParseMapData() {
-            for (int i = 0; i < TOTAL_MAP_COUNT; ++i) {
+            for (int i = 0; i < Constants.TOTAL_MAP_COUNT; ++i) {
                 caves[i] = new Cave(String.Format("Map {0} title", i + 1));
-                caves[i].mapData = new byte[MAP_DATA_BYTE_COUNT];
-                Array.Copy(rawMapData, i * MAP_DATA_BYTE_COUNT, caves[i].mapData, 0, MAP_DATA_BYTE_COUNT);
+                caves[i].mapData = new byte[Constants.MAP_DATA_BYTE_COUNT];
+                Array.Copy(rawMapData, i * Constants.MAP_DATA_BYTE_COUNT, caves[i].mapData, 0, Constants.MAP_DATA_BYTE_COUNT);
             }
         }
 
         private void PopulateMapList() {
-            for (int i = 0; i < TOTAL_MAP_COUNT; ++i) {
+            for (int i = 0; i < Constants.TOTAL_MAP_COUNT; ++i) {
                 var item = listCaves.Items.Add(String.Format("{0}", i + 1));
                 item.SubItems.Add(caves[i].Name);
             }
@@ -289,19 +142,19 @@ namespace tensor_patcher_gui {
         }
 
         private void CreateMapTiles() {
-            for (int i = 0; i < MAP_DIMENSION; ++i) {
-                for (int j = 0; j < MAP_DIMENSION; ++j) {
+            for (int i = 0; i < Constants.MAP_DIMENSION; ++i) {
+                for (int j = 0; j < Constants.MAP_DIMENSION; ++j) {
                     System.Windows.Forms.Button but = new System.Windows.Forms.Button();
-                    but.Top = 32 + i * TILE_SIZE;
-                    but.Left = 32 + j * TILE_SIZE;
-                    but.Height = TILE_SIZE;
-                    but.Width = TILE_SIZE;
+                    but.Top = 32 + i * Constants.TILE_SIZE;
+                    but.Left = 32 + j * Constants.TILE_SIZE;
+                    but.Height = Constants.TILE_SIZE;
+                    but.Width = Constants.TILE_SIZE;
                     but.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
                     but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
                     but.Visible = false;
                     but.MouseEnter += new System.EventHandler(this.button_MapTileMouseEnter);
                     but.MouseLeave += new System.EventHandler(this.button_MapTileMouseLeave);
-                    but.Tag = i * MAP_DIMENSION + j;
+                    but.Tag = i * Constants.MAP_DIMENSION + j;
                     mapTiles[(int)but.Tag] = but;
                     this.Controls.Add(but);
                 }
@@ -312,8 +165,8 @@ namespace tensor_patcher_gui {
             System.Windows.Forms.Button but = new System.Windows.Forms.Button();
             but.Top = y;
             but.Left = x;
-            but.Height = TOOLBOX_TILE_SIZE;
-            but.Width = Convert.ToInt32(TOOLBOX_TILE_SIZE * scale);
+            but.Height = Constants.TOOLBOX_TILE_SIZE;
+            but.Width = Convert.ToInt32(Constants.TOOLBOX_TILE_SIZE * scale);
             but.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             but.BackgroundImage = pic;
@@ -329,42 +182,42 @@ namespace tensor_patcher_gui {
         }
 
         private void CreateToolbox() {
-            var toolboxYPosition = Convert.ToInt32(458 + 2.5 * (TOOLBOX_TILE_SIZE + 4));
-            AddToolboxButton(660 + 0 * (TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.ludek1)
+            var toolboxYPosition = Convert.ToInt32(458 + 2.5 * (Constants.TOOLBOX_TILE_SIZE + 4));
+            AddToolboxButton(660 + 0 * (Constants.TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.ludek1)
                 .Click += new System.EventHandler(this.button_ToolboxButton_Click);
-            AddToolboxButton(660 + 1 * (TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.obstacle00)
+            AddToolboxButton(660 + 1 * (Constants.TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.obstacle00)
                 .Click += new System.EventHandler(this.button_ToolboxButton_Click);
-            AddToolboxButton(660 + 2 * (TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.obstacle01)
+            AddToolboxButton(660 + 2 * (Constants.TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.obstacle01)
                 .Click += new System.EventHandler(this.button_ToolboxButton_Click);
-            AddToolboxButton(660 + 3 * (TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.amygdala7)
+            AddToolboxButton(660 + 3 * (Constants.TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.amygdala7)
                 .Click += new System.EventHandler(this.button_ToolboxButton_Click);
 
-            AddToolboxButton(660 + 4 * (TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.brick_brown, 0.66f)
+            AddToolboxButton(660 + 4 * (Constants.TOOLBOX_TILE_SIZE + 4), toolboxYPosition, Properties.Resources.brick_brown, 0.66f)
                 .Click += new System.EventHandler(this.button_SetBrownBrickSet_Click);
-            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f) * (TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_pink, 0.66f)
+            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f) * (Constants.TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_pink, 0.66f)
                 .Click += new System.EventHandler(this.button_SetPinkBrickSet_Click);
-            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f * 2) * (TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_blue, 0.66f)
+            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f * 2) * (Constants.TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_blue, 0.66f)
                 .Click += new System.EventHandler(this.button_SetBlueBrickSet_Click);
-            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f * 3) * (TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_amygdala, 0.66f)
+            AddToolboxButton(Convert.ToInt32(660 + (4 + 0.75f * 3) * (Constants.TOOLBOX_TILE_SIZE + 4)), toolboxYPosition, Properties.Resources.brick_amygdala, 0.66f)
                 .Click += new System.EventHandler(this.button_SetAmygdalaBrickSet_Click);
 
             AddToolboxBrickButtons();
         }
 
         private void RepaintBrickToolset() {
-            for (int j = 0; j < TOTAL_BRICK_ROWS; ++j) {
-                for (int i = 0; i < TOTAL_BRICKS / TOTAL_BRICK_ROWS; ++i) {
-                    var index = i + j * (TOTAL_BRICKS / TOTAL_BRICK_ROWS);
+            for (int j = 0; j < Constants.TOTAL_BRICK_ROWS; ++j) {
+                for (int i = 0; i < Constants.TOTAL_BRICKS / Constants.TOTAL_BRICK_ROWS; ++i) {
+                    var index = i + j * (Constants.TOTAL_BRICKS / Constants.TOTAL_BRICK_ROWS);
                     toolboxBrickButtons[index].BackgroundImage = currentBrickSet[index];
                 }
             }
         }
 
         private void AddToolboxBrickButtons() {
-            for (int j = 0; j < TOTAL_BRICK_ROWS; ++j) {
-                for (int i = 0; i < TOTAL_BRICKS / TOTAL_BRICK_ROWS; ++i) {
-                    var index = i + j * (TOTAL_BRICKS / TOTAL_BRICK_ROWS);
-                    Button b = AddToolboxButton(660 + i * (TOOLBOX_TILE_SIZE + 4), 458 + j * (TOOLBOX_TILE_SIZE + 4), currentBrickSet[index]);
+            for (int j = 0; j < Constants.TOTAL_BRICK_ROWS; ++j) {
+                for (int i = 0; i < Constants.TOTAL_BRICKS / Constants.TOTAL_BRICK_ROWS; ++i) {
+                    var index = i + j * (Constants.TOTAL_BRICKS / Constants.TOTAL_BRICK_ROWS);
+                    Button b = AddToolboxButton(660 + i * (Constants.TOOLBOX_TILE_SIZE + 4), 458 + j * (Constants.TOOLBOX_TILE_SIZE + 4), currentBrickSet[index]);
                     b.Click += new System.EventHandler(this.button_ToolboxButton_Click);
                     toolboxBrickButtons[index] = b;
                 }
@@ -372,11 +225,11 @@ namespace tensor_patcher_gui {
         }
 
         private void ShowMapLayout(int index) {
-            for (int i = 0; i < MAP_DIMENSION; ++i) {
-                for (int j = 0; j < MAP_DIMENSION; ++j) {
-                    var c = i * MAP_DIMENSION + j;
+            for (int i = 0; i < Constants.MAP_DIMENSION; ++i) {
+                for (int j = 0; j < Constants.MAP_DIMENSION; ++j) {
+                    var c = i * Constants.MAP_DIMENSION + j;
                     var myByte = caves[index].mapData[c];
-                    mapTiles[c].BackgroundImage = caveByteMap[caves[index].mapData[c]];
+                    mapTiles[c].BackgroundImage = Constants.caveByteMap[caves[index].mapData[c]];
                     mapTiles[c].Visible = true;
                 }
             }
